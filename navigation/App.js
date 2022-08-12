dc4002523d4e9b110dce1c8fd130b5b8ce1e5950
Haph762 <phanha762@gmail.com>
2022-08-12 22:48:33 +0700

    screen register

--- navigation/App.js
+++ navigation/App.js
@@ -12,7 +12,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 // import {StackRouter} from 'react-navigation';
-import {Welcome, Login} from '../screens';
+import {Welcome, Login, Register} from '../screens';
 /**
  - Call API(Application Programming Interface)
  - POST, GET, PUT, DELETE
@@ -37,8 +37,8 @@ function App(props) {
           headerShown: false,
         }}>
         {/* <Stack.Screen name={'Welcome'} component={Welcome} /> */}
-        <Stack.Screen name={'Login'} component={Login} />
-        {/* <Stack.Screen name={'Register'} component={Register} /> */}
+        {/* <Stack.Screen name={'Login'} component={Login} /> */}
+        <Stack.Screen name={'Register'} component={Register} />
         {/* <Stack.Screen name={'UITab'} component={UITab} /> */}
         {/* <Stack.Screen name={'Messenger'} component={Messenger} /> */}
       </Stack.Navigator>
---
+++ screens/Register.js
@@ -0,0 +1,243 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  KeyboardAvoidingView,
+  Keyboard,
+} from 'react-native';
+import {images, color, fontSizes, icons, colors} from '../constants';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {isValidEmail, isValidPassword} from '../utilities/Validations';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+function Register(props) {
+  const [keyboardIsShow, setKeyboardIsShow] = useState(false);
+  //sate for validating
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+  const [reErrorPassword, setReErrorPassword] = useState('');
+  //sate to store email/password
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // disable button login
+  const isValidationOK = () => {
+    return (
+      email.length > 0 &&
+      password.length > 0 &&
+      isValidEmail(email) &&
+      isValidPassword(password)
+    );
+  };
+  useEffect(() => {
+    // xử lý khi bàn phím hiện lên các button và register now không bị đè lên nhau
+    Keyboard.addListener('keyboardDidShow', () => {
+      setKeyboardIsShow(true);
+    });
+    Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardIsShow(false);
+    });
+  });
+  return (
+    <KeyboardAwareScrollView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{
+        flex: 100,
+        backgroundColor: colors.primary,
+      }}>
+      <View
+        style={{
+          flex: 20,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: fontSizes.h2,
+            fontWeight: 'bold',
+            width: '50%',
+          }}>
+          Already have an Account?
+        </Text>
+        <Image
+          tintColor={'white'}
+          source={images.computer}
+          style={{width: 100, height: 100, alignSelf: 'center'}}
+        />
+      </View>
+      <View
+        style={{
+          flex: 60,
+          backgroundColor: 'white',
+          padding: 10,
+          margin: 10,
+          borderRadius: 20,
+        }}>
+        <View style={{marginHorizontal: 5}}>
+          <Text style={{color: colors.primary, fontSize: fontSizes.h6}}>
+            Email:{' '}
+          </Text>
+          <TextInput
+            onChangeText={text => {
+              setErrorEmail(
+                isValidEmail(text) == true ? '' : 'Email not in correct format',
+              );
+              setEmail(text);
+            }}
+            style={{
+              color: 'black',
+            }}
+            placeholder="Example@gmail.com"
+            placeholderTextColor={colors.placeholder}
+          />
+          <View
+            style={{
+              height: 1,
+              backgroundColor: colors.primary,
+              width: '100%',
+              marginBottom: 10,
+              marginHorizontal: 10,
+              alignSelf: 'center',
+            }}></View>
+          <Text
+            style={{color: 'red', fontSize: fontSizes.h6, marginBottom: 15}}>
+            {errorEmail}
+          </Text>
+        </View>
+        <View style={{marginHorizontal: 5}}>
+          <Text style={{color: colors.primary, fontSize: fontSizes.h6}}>
+            Password:{' '}
+          </Text>
+          <TextInput
+            onChangeText={password => {
+              setErrorPassword(
+                isValidPassword(password) == true
+                  ? ''
+                  : 'Password must be at least 3 characters',
+              );
+              setPassword(password);
+            }}
+            style={{
+              color: 'black',
+            }}
+            placeholder="Password"
+            placeholderTextColor={colors.placeholder}
+            secureTextEntry={true}
+          />
+          <View
+            style={{
+              height: 1,
+              backgroundColor: colors.primary,
+              width: '100%',
+              marginBottom: 10,
+              marginHorizontal: 10,
+              alignSelf: 'center',
+            }}></View>
+          <Text
+            style={{color: 'red', fontSize: fontSizes.h6, marginBottom: 15}}>
+            {errorPassword}
+          </Text>
+        </View>
+        <View style={{marginHorizontal: 5}}>
+          <Text style={{color: colors.primary, fontSize: fontSizes.h6}}>
+            Retype Password:{' '}
+          </Text>
+          <TextInput
+            onChangeText={password => {
+              setReErrorPassword(
+                isValidPassword(password) == true
+                  ? ''
+                  : 'Password must be at least 3 characters',
+              );
+              setPassword(password);
+            }}
+            style={{
+              color: 'black',
+            }}
+            placeholder="Retype Enter Password"
+            placeholderTextColor={colors.placeholder}
+            secureTextEntry={true}
+          />
+          <View
+            style={{
+              height: 1,
+              backgroundColor: colors.primary,
+              width: '100%',
+              marginBottom: 10,
+              marginHorizontal: 10,
+              alignSelf: 'center',
+            }}></View>
+          <Text style={{color: 'red', fontSize: fontSizes.h6, marginBottom: 5}}>
+            {reErrorPassword}
+          </Text>
+        </View>
+        <TouchableOpacity
+          disabled={isValidationOK() == false}
+          onPress={() => {
+            alert(`email: ${email}, pass: ${password}`);
+          }}
+          style={{
+            backgroundColor: isValidationOK()
+              ? colors.primary
+              : colors.inactive,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '50%',
+            alignSelf: 'center',
+            borderRadius: 15,
+          }}>
+          <Text style={{padding: 8, fontSize: fontSizes.h5, color: 'white'}}>
+            Register
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {keyboardIsShow === false && (
+        <View style={{flex: 20}}>
+          <View
+            style={{
+              height: 40,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginHorizontal: 20,
+            }}>
+            <View
+              style={{
+                height: 1,
+                backgroundColor: 'white',
+                flex: 1,
+              }}></View>
+            <Text
+              style={{
+                padding: 8,
+                fontSize: fontSizes.h6,
+                color: 'white',
+                alignSelf: 'center',
+                marginHorizontal: 5,
+              }}>
+              User other methods ?
+            </Text>
+            <View
+              style={{
+                height: 1,
+                backgroundColor: 'white',
+                flex: 1,
+              }}></View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}>
+            <Icon name="facebook" size={35} color={colors.facebook}></Icon>
+            <View style={{width: 15}}></View>
+            <Icon name="google" size={35} color={colors.google}></Icon>
+          </View>
+        </View>
+      )}
+    </KeyboardAwareScrollView>
+  );
+}
+export default Register;
